7c8445ee828193419dd6c5ec1068e42806336362
dinesh93900 <dinesh14701@gmail.com>
2024-06-16 05:11:29 +0000

    updated

---
+++ devera.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+expr 1 + 1
+expr 4 - 2
+expr 2 \* 1
+expr 4 / 2
+expr 4 % 2
+
+
+
+
+a=1
+b=1
+c=1
+name=dinesh
+echo "the variable a value is : $a"
+echo "the variable b value is : $b"
+echo "the variable c value is : $c"
+echo "the name  of the variable is : $name"
+echo $0
+echo $1
+echo $2
+echo $3
---
+++ samba.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "hello everyone "
+echo "iam human"
+echo "writing shellscripting"
+echo "single linw command"
+echo "multiline command"
+echo "today date is "
+date
+<<dinesh
+singlr line comments
+multiline comments
+variables
+arethemetic operations
+command line arguments
+dinesh
+
