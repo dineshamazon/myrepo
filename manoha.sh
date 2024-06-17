237d3c3d7a9221c0eef6a0f1dc9602b7872bcf09
dinesh93900 <dinesh14701@gmail.com>
2024-06-17 08:14:22 +0000

    commit 1

---
+++ manoha.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+echo "iam dinesh"
+echo "iam writing shellscripting"
+echo "today is frist class "
+#go through is 
+<<dinesh
+hello 
+haii
+dineshk
+wishes
+dinesh
+a=1
+b=1
+
+c=1
+name=dinesh
+echo "the variable a value is : $a"
+echo "the variable b value is : $b"
+echo "the variable c value is : $c"
+echo "the variable name is : $name"
+
+expr 2 + 2
+expr 4 - 2
+expr 2 \* 1
+expr 2 / 1
+expr 2 % 1
+echo $1
+echo $0
+echo $2
+echo $3
+
