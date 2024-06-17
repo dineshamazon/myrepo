27ce45daefb8bcdc879eec1620d3340e6bf0ba7d
dinesh93900 <dinesh14701@gmail.com>
2024-06-17 05:01:48 +0000

    commited

---
+++ father.sh
@@ -0,0 +1,4 @@
+echo hello everyone 
+iam dinesh 
+please come to class regularly
+dont miss any class
---
+++ mother.sh
@@ -0,0 +1,5 @@
+echo hello everyone 
+iam dinesh 
+please come to class regularly
+dont miss any class
+
